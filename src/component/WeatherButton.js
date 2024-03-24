020837826a5ac112abd57fd4083cba2c996c5d9a
hellojoyworldz <hellojoyworldz@gmail.com>
2024-03-24 18:17:50 +0900

    도시버튼 클라스 및 스타일 추가

--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const WeatherButton = ({cities, getCurrentLocation, setCity}) => {
   return (
-    <div>
-      <button onClick={getCurrentLocation}>Current Location</button>
+    <div className="city">
+      <button type="button" className="city-button active" onClick={getCurrentLocation}>Current Location</button>
       {
         cities.map((item,idx) => {
-          return <button key={idx} onClick={() => setCity(item)}>{item}</button>
+          return <button type="button" className="city-button" onClick={() => setCity(item)}>{item}</button>
         })
       }
     </div>
