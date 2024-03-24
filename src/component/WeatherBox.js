a24ddb1d3afda22e7480746a0fb95e798db0c6d5
hellojoyworldz <hellojoyworldz@gmail.com>
2024-03-24 21:42:46 +0900

    이미지 기본사이즈 설정

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -4,7 +4,7 @@ const WeatherBox = ({weather, flag}) => {
 
   let c = Math.floor(weather?.main.temp)
   let f = Math.floor(9/5*(weather?.main.temp)+32)
-  let icon = <img src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} alt="" />
+  let icon = <img src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} width="100" height="100" alt="" />
 
   return (
     <div className="info">
