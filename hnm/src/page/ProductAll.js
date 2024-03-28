080b010daac1de183dabb3ffb567f21924b1c852
hellojoyworldz <hellojoyworldz@gmail.com>
2024-03-29 00:43:50 +0900

    api url 변경, 메인 total 추가 및 스타일 class 추가

--- hnm/src/page/ProductAll.js
+++ hnm/src/page/ProductAll.js
@@ -5,10 +5,12 @@ import ProductCard from "../component/ProductCard";
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
   const getProduct = async () => {
-    let url = "http://localhost:3004/products";
+    // let url = "http://localhost:3004/products";
+    let url =
+      "https://gist.githubusercontent.com/legobitna/24cf11ae95d53c9cdcdc7b0040f059e9/raw/e55a200f0461fb1ba866a9fc86332b27e234d5a0/db.json";
     let response = await fetch(url);
     let data = await response.json();
-    setProductList(data);
+    setProductList(data.products);
   };
   useEffect(() => {
     getProduct();
@@ -16,10 +18,11 @@ const ProductAll = () => {
   return (
     <div>
       <Container>
+        <div className="mb-4">Total: {productList.length}</div>
         <Row>
           {productList.map((menu, idx) => (
-            <Col lg={3}>
-              <ProductCard item={menu} key={idx} />
+            <Col lg={3} md={6} sm={12} key={idx} className="mb-4">
+              <ProductCard item={menu} />
             </Col>
           ))}
         </Row>
