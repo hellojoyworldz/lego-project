a0bf6a4fbb02eb5cb4ea84459eb1fc34e2dfbf67
hellojoyworldz <hellojoyworldz@gmail.com>
2024-03-29 00:42:12 +0900

    상품 디테일

--- hnm/src/page/ProductDetail.js
+++ hnm/src/page/ProductDetail.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { Container, Row, Col, Dropdown, Button } from "react-bootstrap";
 
 const ProductDetail = () => {
-  return <div>ProductDetail</div>;
+  return (
+    <div className="card-detail">
+      <Container>
+        <Row>
+          <Col>
+            <img
+              src="https://noona-hnm.netlify.app/pattern-jacket.jpeg"
+              widt={564}
+              height={846}
+              alt=""
+            />
+          </Col>
+          <Col>
+            <div className="m-2 display-6 ">title</div>
+            <div className="price m-2">price</div>
+            <div className="choice m-2">choice</div>
+            <Dropdown className="m-2">
+              <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
+                Sizes
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <Button className="w-100 m-2" variant="dark">
+              Add to Bag
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default ProductDetail;
