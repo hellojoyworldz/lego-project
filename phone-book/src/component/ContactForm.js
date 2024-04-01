88aca874af8bab8843bbfac82b40db036d5b056b
hellojoyworldz <hellojoyworldz@gmail.com>
2024-04-02 02:11:11 +0900

    ContactForm.js  - import useEffect 제거

--- phone-book/src/component/ContactForm.js
+++ phone-book/src/component/ContactForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import InputText from "./InputText";
 import Button from "./Button";
 import { useDispatch } from "react-redux";
