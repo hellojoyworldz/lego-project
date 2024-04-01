bdf1fd4e7a43bbed088279a48ee61821c97c2a2f
hellojoyworldz <hellojoyworldz@gmail.com>
2024-04-02 03:11:52 +0900

    로고 클릭 시 메인으로

--- phone-book/src/App.js
+++ phone-book/src/App.js
@@ -5,7 +5,9 @@ import ContactList from "./component/ContactList";
 function App() {
   return (
     <div className="container pt-8 pb-8 mx-auto">
-      <h1 className="mb-8 text-5xl font-bold text-center">Phone Book</h1>
+      <h1 className="mb-8 text-5xl font-bold text-center">
+        <a href="/">Phone Book</a>
+      </h1>
       <div className="flex flex-row gap-6">
         <div className="basis-1/2">
           <ContactForm />
