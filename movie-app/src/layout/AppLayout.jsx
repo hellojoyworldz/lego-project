ea336b424080107fe5bc23e56ecf5352970506cf
hellojoyworldz <hellojoyworldz@gmail.com>
2024-04-07 01:03:39 +0900

    AppLayout 만들기

--- movie-app/src/layout/AppLayout.jsx
+++ movie-app/src/layout/AppLayout.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import { Outlet } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const AppLayout = () => {
-  return <div>navbar</div>;
+  return (
+    <div>
+      <Navbar expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="/">
+            <img
+              src="https://about.netflix.com/images/logo.png"
+              style={{ height: "60px" }}
+              alt=""
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="my-2 me-auto my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/movies">Movies</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                variant="outline-danger"
+              />
+              <Button variant="outline-danger">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Outlet />
+    </div>
+  );
 };
 
 export default AppLayout;
