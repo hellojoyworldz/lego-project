1a0f036008f4c3e11c0074a97a24bd291490e39b
hellojoyworldz <hellojoyworldz@gmail.com>
2024-04-07 00:06:18 +0900

    Route 구성하기

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import AppLayout from "./layout/AppLayout";
+import Homepage from "./pages/Homepage/Homepage";
+import MoviePage from "./pages/Movies/MoviePage";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Homepage />} />
+        <Route path="movies">
+          <Route index element={<MoviePage />} />
+          <Route path=":id" elelment={MovieDetailPage} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
