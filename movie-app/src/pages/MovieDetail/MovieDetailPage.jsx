8ca5417dd6417524bb238d81fc290d274b9a3864
hellojoyworldz <hellojoyworldz@gmail.com>
2024-04-12 23:01:32 +0900

    MovieDetailPage - 데이터 가져오기,  Banner 붙이기

--- movie-app/src/pages/MovieDetail/MovieDetailPage.jsx
+++ movie-app/src/pages/MovieDetail/MovieDetailPage.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useMovieDetailQuery } from "../../hooks/useMovieDetail";
+import Alert from "react-bootstrap/Alert";
+import Lodingspinner from "../../common/components/Lodings/Lodings";
+import Banner from "../../common/components/Banner/Banner";
 
 const MovieDetailPage = () => {
-  return <div>MovieDetailPage</div>;
+  const { id } = useParams();
+
+  const { data, isLoading, isError, error } = useMovieDetailQuery(id);
+
+  if (isLoading) {
+    return <Lodingspinner />;
+  }
+
+  if (isError) {
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
+
+  return (
+    <div className="movie-detail">
+      <Banner
+        imgpath={data?.backdrop_path}
+        title={data.title}
+        overview={data.tagline}
+      />
+    </div>
+  );
 };
 
 export default MovieDetailPage;
