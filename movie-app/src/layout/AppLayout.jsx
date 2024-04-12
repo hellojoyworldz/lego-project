7c24fba3d093286e1827859b49cad61366c01d75
hellojoyworldz <hellojoyworldz@gmail.com>
2024-04-12 20:32:04 +0900

    footer

--- movie-app/src/layout/AppLayout.jsx
+++ movie-app/src/layout/AppLayout.jsx
@@ -15,45 +15,49 @@ const AppLayout = () => {
     navigate(`/movies?q=${keyword}`);
   };
   return (
-    <main>
-      <Navbar expand="lg" className="navbar">
-        <Container fluid>
-          <Navbar.Brand href="/">
-            <img src="/images/logo.png" style={{ height: "44px" }} alt="" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="my-2 me-auto my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll>
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/movies">
-                Movies
-              </Nav.Link>
-            </Nav>
-            <Form className="d-flex" onSubmit={searchByKeyword}>
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-                variant="outline-danger"
-                value={keyword}
-                onChange={(event) => setKeyword(event.target.value)}
-              />
-              <Button variant="outline-danger" type="submit">
-                Search
-              </Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
-      <Outlet />
-    </main>
+    <>
+      <header>
+        <Navbar expand="lg" className="navbar">
+          <Container fluid>
+            <Navbar.Brand href="/">
+              <img src="/images/logo.png" style={{ height: "44px" }} alt="" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="my-2 me-auto my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll>
+                <Nav.Link as={Link} to="/">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/movies">
+                  Movies
+                </Nav.Link>
+              </Nav>
+              <Form className="d-flex" onSubmit={searchByKeyword}>
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                  variant="outline-danger"
+                  value={keyword}
+                  onChange={(event) => setKeyword(event.target.value)}
+                />
+                <Button variant="outline-danger" type="submit">
+                  Search
+                </Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer className="main-footer">Movick Korea</footer>
+    </>
   );
 };
 
