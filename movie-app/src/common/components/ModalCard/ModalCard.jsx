1a3459b822b53a85b8dc85887f0415043448293d
hellojoyworldz <hellojoyworldz@gmail.com>
2024-04-13 12:25:29 +0900

    youtube-react 세팅 및 ModalCard 컴포넌트 생성

---
+++ movie-app/src/common/components/ModalCard/ModalCard.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import YouTube from "react-youtube";
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered>
+      <Modal.Header closeButton>
+        {props.modalTitle && (
+          <Modal.Title id="contained-modal-title-vcenter">
+            {props.modalTitle}
+          </Modal.Title>
+        )}
+      </Modal.Header>
+      <Modal.Body>
+        {props.contentTitle && <h4>{props.contentTitle}</h4>}
+        {props.youtube && <YouTube />}
+        {props.contentText && <p>{props.contentText}</p>}
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+const ModalCard = ({ title, youtube }) => {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <>
+      <Button variant="danger" onClick={() => setModalShow(true)}>
+        {title}
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        youtube={youtube}
+      />
+    </>
+  );
+};
+
+export default ModalCard;
