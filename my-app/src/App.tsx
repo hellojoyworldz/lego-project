9cf2d5ba2cab7b812ac9bd638813d3fdebd1f2ad
joy <116714457+hellojoyworldz@users.noreply.github.com>
2025-06-18 15:54:34 +0900

    feat: add slide page transitions using framer-motion

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,42 @@
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import HomeContainer from './containers/HomeContainer';
+import AboutContainer from './containers/AboutContainer';
+import PageTransition from './components/PageTransition';
 import './i18n';
 
+const AnimatedRoutes = () => {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <PageTransition>
+              <HomeContainer />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <PageTransition>
+              <AboutContainer />
+            </PageTransition>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
+  );
+};
+
 function App() {
-  return <HomeContainer />;
+  return (
+    <BrowserRouter>
+      <AnimatedRoutes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
