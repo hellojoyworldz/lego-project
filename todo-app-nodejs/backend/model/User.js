b1e717794cc94f83c513c52d7337f7e66ff452cf
hellojoyworldz <hellojoyworldz@gmail.com>
2025-07-27 21:02:04 +0900

    [todo-app-nodejs] feat: backend - 회원가입 및 로그인 기능 추가 - README.md에 회원가입 및 로그인 기능 설명 추가 - bcryptjs 및 jsonwebtoken 패키지 추가 - 유저 API 라우터 추가

---
+++ todo-app-nodejs/backend/model/User.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.generateToken = function () {
+  const token = jwt.sign({ _id: this._id }, JWT_SECRET, {
+    expiresIn: "1d",
+  });
+  return token;
+};
+
+userSchema.methods.toJSON = function () {
+  const object = this._doc;
+  delete object.password;
+
+  return object;
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- todo-app-nodejs/backend/routes/index.js
+++ todo-app-nodejs/backend/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const taskApi = require("./task.api.js");
+const userApi = require("./user.api.js");
 
 router.use("/tasks", taskApi);
+router.use("/user", userApi);
 
 module.exports = router;
