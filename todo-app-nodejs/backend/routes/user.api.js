8ff368137de53281ee06798286759a725acd18b5
hellojoyworldz <hellojoyworldz@gmail.com>
2025-07-27 23:16:48 +0900

    [todo-app-nodejs] fix: backend - 로그인 함수 오탈자 수정

--- todo-app-nodejs/backend/routes/user.api.js
+++ todo-app-nodejs/backend/routes/user.api.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require("../controller/user.controller");
 
 router.post("/", userController.createUser);
-router.post("/login", userController.loginWidthEmail);
+router.post("/login", userController.loginWithEmail);
 
 module.exports = router;
