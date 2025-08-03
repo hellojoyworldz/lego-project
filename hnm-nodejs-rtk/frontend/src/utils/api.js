084593e48e87ea0ed76ced5662f3b3685380eb4b
hellojoyworldz <hellojoyworldz@gmail.com>
2025-08-03 09:21:49 +0900

    [hnm-nodejs-rtk] fix: frontend - API 리다이렉트 URL 수정

--- hnm-nodejs-rtk/frontend/src/utils/api.js
+++ hnm-nodejs-rtk/frontend/src/utils/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 // 상황따라 주소 다름
-const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
 const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
-// const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
+const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 // console.log("proxy", BACKEND_PROXY);
 
 const URL =
-  process.env.NODE_ENV === "production" ? PROD_BACKEND : LOCAL_BACKEND;
+  process.env.NODE_ENV === "production" ? PROD_BACKEND : BACKEND_PROXY;
 const api = axios.create({
   baseURL: URL,
   headers: {
