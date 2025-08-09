0b18320a2ce8486eee6f068a43f988976c2b2709
hellojoyworldz <hellojoyworldz@gmail.com>
2025-08-09 11:09:33 +0900

    feat: 랜딩페이지에서 검색 쿼리 및 페이지네이션 기능 추가

--- hnm-nodejs-rtk/frontend/src/page/LandingPage/LandingPage.js
+++ hnm-nodejs-rtk/frontend/src/page/LandingPage/LandingPage.js
@@ -5,21 +5,55 @@ import { useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductList } from "../../features/product/productSlice";
 import LoadingSpinner from "../../common/component/LoadingSpinner";
+import ReactPaginate from "react-paginate";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const productList = useSelector((state) => state.product.productList);
+  const totalPageNum = useSelector((state) => state.product.totalPageNum);
   const loading = useSelector((state) => state.product.loading);
   const [query] = useSearchParams();
-  const name = query.get("name");
+  const [searchQuery, setSearchQuery] = useState({
+    page: query.get("page") || 1,
+    name: query.get("name") || "",
+  });
+
   useEffect(() => {
     dispatch(
       getProductList({
-        name,
+        ...searchQuery,
       })
     );
-  }, [name, dispatch]);
+  }, [dispatch, searchQuery]);
+
+  useEffect(() => {
+    setSearchQuery({
+      page: query.get("page") || 1,
+      name: query.get("name") || "",
+    });
+  }, [query]);
+
+  useEffect(() => {
+    if (searchQuery.name === "") {
+      delete searchQuery.name;
+    }
+
+    if (searchQuery.page === "") {
+      delete searchQuery.page;
+    }
+
+    const params = new URLSearchParams(searchQuery);
+    const query = params.toString();
+    navigate(`?${query}`);
+  }, [searchQuery, navigate]);
+
+  const handlePageClick = ({ selected }) => {
+    setSearchQuery({ ...searchQuery, page: selected + 1 });
+  };
 
   return (
     <Container>
@@ -27,17 +61,41 @@ const LandingPage = () => {
         {loading ? (
           <LoadingSpinner />
         ) : productList.length > 0 ? (
-          productList.map((item) => (
-            <Col md={3} sm={12} key={item._id}>
-              <ProductCard item={item} />
-            </Col>
-          ))
+          <>
+            {productList.map((item) => (
+              <Col md={3} sm={12} key={item._id}>
+                <ProductCard item={item} />
+              </Col>
+            ))}
+
+            <ReactPaginate
+              nextLabel="next >"
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={5}
+              pageCount={totalPageNum}
+              forcePage={searchQuery.page - 1}
+              previousLabel="< previous"
+              renderOnZeroPageCount={null}
+              pageClassName="page-item"
+              pageLinkClassName="page-link"
+              previousClassName="page-item"
+              previousLinkClassName="page-link"
+              nextClassName="page-item"
+              nextLinkClassName="page-link"
+              breakLabel="..."
+              breakClassName="page-item"
+              breakLinkClassName="page-link"
+              containerClassName="pagination"
+              activeClassName="active"
+              className="display-center list-style-none"
+            />
+          </>
         ) : (
           <div className="text-align-center empty-bag">
-            {name === "" ? (
+            {searchQuery.name === "" ? (
               <h2>등록된 상품이 없습니다!</h2>
             ) : (
-              <h2>{name}과 일치한 상품이 없습니다!`</h2>
+              <h2>{searchQuery.name}과 일치한 상품이 없습니다!`</h2>
             )}
           </div>
         )}
